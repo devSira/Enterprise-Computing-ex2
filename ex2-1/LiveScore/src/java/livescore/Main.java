cf02e7b23f51920e4c54f755216cd01b11c0f3a6
devSira <113962354+devSira@users.noreply.github.com>
2023-02-14 01:16:41 +0700

    Add files via upload

---
+++ ex2-1/LiveScore/src/java/livescore/Main.java
@@ -0,0 +1,85 @@
+package livescore;
+
+import java.util.Scanner;
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+public class Main {
+
+    @Resource(mappedName = "jms/SimpleJMSTopic")
+    private static Topic topic;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    @Resource(mappedName = "jms/SimpleJMSQueue")
+    private static Queue queue;
+
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        if ((args.length < 1) || (args.length > 2)) {
+            System.err.println(
+                    "Program takes one or two arguments: "
+                    + "<dest_type> [<number-of-messages>]");
+            System.exit(1);
+        }
+
+        String destType = args[0];
+        System.out.println("Destination type is " + destType);
+
+        if (!(destType.equals("queue") || destType.equals("topic"))) {
+            System.err.println("Argument must be \"queue\" or " + "\"topic\"");
+            System.exit(1);
+        }
+        Destination dest = null;
+
+        try {
+            if (destType.equals("queue")) {
+                dest = (Destination) queue;
+            } else {
+                dest = (Destination) topic;
+            }
+        } catch (Exception e) {
+            System.err.println("Error setting destination: " + e.toString());
+            System.exit(1);
+        }
+
+        try {
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            MessageProducer producer = session.createProducer(dest);
+            TextMessage message = session.createTextMessage();
+            System.out.println("To end program, press Enter " + "then <return>");
+            Scanner input = new Scanner(System.in);
+            while (true) {
+                System.out.print("Enter Live Score: ");
+                String score = input.nextLine();
+                if (score.equals("")) {
+                    break;
+                }
+                message.setText(score);
+                producer.send(message);
+            }
+            producer.send(session.createMessage());
+
+        } catch (JMSException e) {
+            System.err.println("Exception occurred: " + e.toString());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                }
+            }
+        }
+    }
+
+}
